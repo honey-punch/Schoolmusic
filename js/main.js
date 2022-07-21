dc926c326f3f2ff07ee9c57e71a3f2730086e055
mad <mad@jangjicheol.local>
2022-07-21 16:17:38 +0900

    js 수정 및 시멘틱 마크업을 위한 수정

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 // category menu
-const categoryBtn = document.querySelector('.category-btn');
-const categoryArea = document.querySelector('.category-area')
+const categoryBtnOpen = document.querySelector('.category-btn.open');
+const categoryBtnClose = document.querySelector('.category-btn.close');
+const categoryArea = document.querySelector('.category-area');
+const category = document.querySelector('.category');
 
-categoryBtn.addEventListener('click', () => {
-    categoryArea.classList.toggle('grid')
+categoryBtnOpen.addEventListener('click', () => {
+    categoryArea.classList.add('grid');
+    categoryBtnOpen.style.display = 'none';
+    categoryBtnClose.style.display = 'inline';
+    category.style.background = 'var(--dark-grey-color)';
+})
+categoryBtnClose.addEventListener('click', () => {
+    categoryArea.classList.remove('grid');
+    categoryBtnOpen.style.display = 'inline';
+    categoryBtnClose.style.display = 'none';
+    category.style.background = 'var(--main-color)';
 })
 
 // slider
-// 슬라이드 전체 크기(width 구하기)
 const slide = document.querySelector(".banner-list");
 let slideWidth = slide.clientWidth;
 
-// 버튼 엘리먼트 선택하기
 const prevBtn = document.querySelector(".left");
 const nextBtn = document.querySelector(".right");
 
-// 슬라이드 전체를 선택해 값을 변경해주기 위해 슬라이드 전체 선택하기
 let slideItems = document.querySelectorAll(".banner-list li");
-// 현재 슬라이드 위치가 슬라이드 개수를 넘기지 않게 하기 위한 변수
 const maxSlide = slideItems.length;
 
 // 버튼 클릭할 때 마다 현재 슬라이드가 어디인지 알려주기 위한 변수
@@ -60,7 +67,6 @@ slideItems.forEach((i) => {
 
 function nextMove() {
   currSlide++;
-  // 마지막 슬라이드 이상으로 넘어가지 않게 하기 위해서
   if (currSlide <= maxSlide) {
     // 슬라이드를 이동시키기 위한 offset 계산
     const offset = slideWidth * currSlide;
@@ -95,7 +101,6 @@ function nextMove() {
 }
 function prevMove() {
   currSlide--;
-  // 1번째 슬라이드 이하로 넘어가지 않게 하기 위해서
   if (currSlide > 0) {
     // 슬라이드를 이동시키기 위한 offset 계산
     const offset = slideWidth * currSlide;
@@ -119,7 +124,6 @@ function prevMove() {
     setTimeout(() => {
       // 각 슬라이드 아이템의 left에 offset 적용
       slideItems.forEach((i) => {
-        // i.setAttribute("style", `transition: ${0}s; left: ${-offset}px`);
         i.setAttribute("style", `transition: ${0.15}s; left: ${-offset}px`);
       });
     }, 0);
@@ -129,25 +133,20 @@ function prevMove() {
   }
 }
 
-// 버튼 엘리먼트에 클릭 이벤트 추가하기
 nextBtn.addEventListener("click", () => {
-  // 이후 버튼 누를 경우 현재 슬라이드를 변경
   nextMove();
 });
-// 버튼 엘리먼트에 클릭 이벤트 추가하기
+
 prevBtn.addEventListener("click", () => {
-  // 이전 버튼 누를 경우 현재 슬라이드를 변경
   prevMove();
 });
 
-// 브라우저 화면이 조정될 때 마다 slideWidth를 변경하기 위해
 window.addEventListener("resize", () => {
   slideWidth = slide.clientWidth;
 });
 
-// 각 페이지네이션 클릭 시 해당 슬라이드로 이동하기
+
 for (let i = 0; i < maxSlide; i++) {
-  // 각 페이지네이션마다 클릭 이벤트 추가하기
   paginationItems[i].addEventListener("click", () => {
     // 클릭한 페이지네이션에 따라 현재 슬라이드 변경해주기(currSlide는 시작 위치가 1이기 때문에 + 1)
     currSlide = i + 1;
@@ -157,23 +156,19 @@ for (let i = 0; i < maxSlide; i++) {
     slideItems.forEach((i) => {
       i.setAttribute("style", `left: ${-offset}px`);
     });
-    // 슬라이드 이동 시 현재 활성화된 pagination 변경
     paginationItems.forEach((i) => i.classList.remove("active"));
     paginationItems[currSlide - 1].classList.add("active");
   });
 }
 
-// 기본적으로 슬라이드 루프 시작하기
 let loopInterval = setInterval(() => {
     nextMove();
   }, 3000);
   
-  // 슬라이드에 마우스가 올라간 경우 루프 멈추기
   slide.addEventListener("mouseover", () => {
     clearInterval(loopInterval);
   });
   
-  // 슬라이드에서 마우스가 나온 경우 루프 재시작하기
   slide.addEventListener("mouseout", () => {
     loopInterval = setInterval(() => {
       nextMove();
@@ -203,3 +198,9 @@ for (let i = 0; i < tabTitleList.length; i++) {
         tabTitleList[i].classList.add('fill-main-color');
     })
 };
+
+// to top
+const toTopBtn = document.querySelector('.to-top');
+toTopBtn.addEventListener('click',function(){
+  window.scrollTo({top: 0, behavior:'smooth'});
+});
