03d30ec7b1b60a9d24c1341c31c67eba02b2abee
mad <mad@jangjicheol.local>
2022-07-28 17:54:16 +0900

    join page validation 기능 추가

--- js/join.js
+++ js/join.js
@@ -33,7 +33,151 @@ categoryBtnClose.addEventListener('mouseout', () => {
 
 // main
 // validation
-const validation = document.querySelector('.validation');
+const userId = document.querySelector('#user-id');
+const password = document.querySelector('#password');
+const passwordAgain = document.querySelector('#password-again');
+const userName = document.querySelector('#user-name');
+const mobile = document.querySelector('#mobile');
+const birth = document.querySelector('#birth');
+
+const man = document.querySelector('#man');
+const woman = document.querySelector('#woman');
+
+const termAgree = document.querySelector('#term-agree');
+const personalAgree = document.querySelector('#personal-agree');
+
+const checkUserId = /^[a-z]+[a-z0-9]{6,18}$/g;
+const checkPassword = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+const checkUserName = /[ㄱ-힣]/;
+const checkMobile = /^\d{10,11}$/;
+const checkBirth = /^\d{8}$/;
+
+// 회원가입하기 버튼 누르면 실행되는 validation 함수
+function joinValidation() {
+  
+  // id
+  if (userId.value == '') {
+    alert(userId.getAttribute('placeholder'));
+    userId.focus();
+    return false;
+  }
+
+  if (userId.value.match(checkUserId) == null) {
+    alert(guideUserId.textContent);
+    userId.focus();
+    return false;
+  }
+
+  // password
+  if (password.value == '') {
+    alert(password.getAttribute('placeholder'));
+    password.focus();
+    return false;
+  }
+
+  if (password.value.match(checkPassword) == null) {
+    alert(guidePassword.textContent);
+    password.focus();
+    return false;
+  }
+
+  if (password.value !== passwordAgain.value) {
+    alert(guidePasswordAgain.textContent);
+    passwordAgain.focus();
+    return false;
+  }
+
+  // name
+  if (userName.value == '') {
+    alert(userName.getAttribute('placeholder'));
+    userName.focus();
+    return false;
+  }
+
+  if (userName.value.match(checkUserName) == null) {
+    alert(guideUserName.textContent);
+    userName.focus();
+    return false;
+  }
+
+  //mobile
+  if (mobile.value == '') {
+    alert(mobile.getAttribute('placeholder'));
+    mobile.focus();
+    return false;
+  }
+
+  if (mobile.value.match(checkMobile) == null) {
+    alert(guideMobile.textContent);
+    mobile.focus();
+    return false;
+  }
+
+  // birth
+  if (birth.value == '') {
+    alert(birth.getAttribute('placeholder'));
+    birth.focus();
+    return false;
+  }
+
+  if (birth.value.match(checkBirth) == null) {
+    alert(guideBirth.textContent);
+    birth.focus();
+    return false;
+  }
+
+  // sex
+  if (!man.checked && !woman.checked) {
+    alert('성별을 체크해주세요.');
+    man.focus();
+    return false;
+  }
+
+  // agree
+  if (!termAgree.checked) {
+    alert('이용약관에 동의해주세요.');
+    termAgree.focus();
+    return false;
+  }
+  if (!personalAgree.checked) {
+    alert('개인정보수집이용에 동의해주세요');
+    personalAgree.focus();
+    return false;
+  }
+
+  document.join.submit();
+}
+
+// 수시로 체크해주어 하단에 문구를 뜨게하는 스크립트
+const guideUserId = document.querySelector('.guide-user-id');
+const guidePassword = document.querySelector('.guide-password');
+const guidePasswordAgain = document.querySelector('.guide-password-again');
+const guideUserName = document.querySelector('.guide-user-name');
+const guideMobile = document.querySelector('.guide-mobile');
+const guideBirth = document.querySelector('.guide-birth');
+
+passwordAgain.addEventListener('input', () => {
+  if (password.value !== passwordAgain.value) {
+    guidePasswordAgain.classList.add('flex');
+  } else {
+    guidePasswordAgain.classList.remove('flex');
+  }
+})
+
+function showGuide(element, check, guide) {
+  element.addEventListener('input', () => {
+    if(element.value.match(check) == null) {
+      guide.classList.add('flex');
+    } else {
+      guide.classList.remove('flex');
+    }
+  })
+}
+showGuide(userId, checkUserId, guideUserId);
+showGuide(password, checkPassword, guidePassword);
+showGuide(userName, checkUserName, guideUserName);
+showGuide(mobile, checkMobile, guideMobile);
+showGuide(birth, checkBirth, guideBirth);
 
 
 
